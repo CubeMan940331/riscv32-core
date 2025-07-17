664e882e7fa3e7bc5b9314b7ecbe15d0331a1523
nototria <humphrey0709@gmail.com>
2025-07-18 00:05:08 +0800

    FEAT: add CSR file support r/w operation

--- CSRFile.v
+++ CSRFile.v
@@ -1,138 +1,282 @@
+//-----------------------------------------------------------------
+//                         RISC-V Core
+//                            V1.0.1
+//                     Ultra-Embedded.com
+//                     Copyright 2014-2019
+//
+//                   admin@ultra-embedded.com
+//
+//                       License: BSD
+//-----------------------------------------------------------------
+//
+// Copyright (c) 2014-2019, Ultra-Embedded.com
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions 
+// are met:
+//   - Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//   - Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer 
+//     in the documentation and/or other materials provided with the 
+//     distribution.
+//   - Neither the name of the author nor the names of its contributors 
+//     may be used to endorse or promote products derived from this 
+//     software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
+// THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+// SUCH DAMAGE.
+//-----------------------------------------------------------------
+/* verilator lint_off UNUSEDSIGNAL */
+`include "riscv_defs.v"
+
 module CSRFile (
-    input         clk,
-    input         rst_n,
+     input clk
+    ,input rst_n
+    
+    ,input  [31:0]  cpu_id_i
+    ,input  [31:0]  misa_i
 
-    // Zicsr access
-    input  [11:0] csr_addr_i,
-    input  [31:0] csr_wdata_i,
-    input         csr_we_i,
-    output reg [31:0] csr_rdata_o,
+    ,input  [5:0]   exception_i
+    ,input  [31:0]  exception_pc_i
+    ,input  [31:0]  exception_addr_i
 
-    // Trap / return
-    input         trap_taken_i,     // assert 1‑cycle when trap is taken
-    input  [31:0] trap_pc_i,        // PC that caused trap
-    input  [31:0] mcause_i,         // trap cause
-    input         mret_i,           // 1‑cycle pulse on MRET
+    ,input          csr_rd_en_i
+    ,input  [11:0]  csr_rd_addr_i
+    ,output [31:0]  csr_rd_data_i
+    
+    ,input  [11:0]  csr_wr_addr_i
+    ,input  [31:0]  csr_wr_data_i
 
-    output [31:0] csr_pc_redirect_o,// next PC (mtvec or mepc)
-    output [1:0]  cur_priv_o        // 00 = U, 11 = M  (debug / external use)
+    ,output         csr_branch_o
+    ,output [31:0]  csr_target_o
+
+    // CSR registers
+    ,output [1:0]   priv_o
+    ,output [31:0]  mstatus_o
+    ,output [31:0]  satp_o
+
+    ,output [31:0]  interrupt_o
 );
-localparam PRIV_U = 2'b00;
-localparam PRIV_M = 2'b11;
 
-///////////////////////////////////////////////////////////////////////////////
-// Registers
-///////////////////////////////////////////////////////////////////////////////
-reg [31:0] mstatus, mie, mtvec, mscratch, mepc, mcause, mtval;
-reg [31:0] utvec,  uscratch, uepc,  ucause,  utval;
-reg        uie;            // bit‑0 only
-reg [1:0]  priv_reg;       // current privilege
+// utilities
+reg [1:0] csr_priv_r;
+reg [1:0] csr_priv_q;
+reg [31:0] csr_satp_r;
+reg [31:0] csr_satp_q;
+reg [31:0]  csr_mip_next_q; // what the hell?
+reg [31:0]  csr_mip_next_r;
 
-assign cur_priv_o = priv_reg;
+// CSR - Machine
 
-///////////////////////////////////////////////////////////////////////////////
-// Helpers to pick out mstatus fields we actually touch
-///////////////////////////////////////////////////////////////////////////////
-wire MIE  = mstatus[3];
-wire MPIE = mstatus[7];
-wire [1:0] MPP = mstatus[12:11];
+// Information RO
+reg [31:0] csr_mvendorid_q;
+reg [31:0] csr_marchid_q;
+reg [31:0] csr_mimpid_q;
+reg [31:0] csr_mhartid_q;
+reg [31:0] csr_mconfigptr_q;
 
-///////////////////////////////////////////////////////////////////////////////
-// Main sequential block
-///////////////////////////////////////////////////////////////////////////////
-always @(posedge clk or negedge rst_n) begin
-    if (!rst_n) begin
-        mstatus <= 0; mie <= 0; mtvec <= 0; mscratch <= 0;
-        mepc <= 0; mcause <= 0; mtval <= 0;
-        utvec <= 0; uscratch <= 0; uepc <= 0; ucause <= 0; utval <= 0;
-        uie <= 1'b0;
-        priv_reg <= PRIV_M;        // start in Machine mode
-    end else begin
-        //----------------------------------------------------------------------
-        // Trap into Machine mode
-        //----------------------------------------------------------------------
-        if (trap_taken_i) begin
-            mepc   <= trap_pc_i;
-            mcause <= mcause_i;
-            mstatus[7]     <= MIE;      // MPIE ← MIE
-            mstatus[3]     <= 1'b0;     // MIE  ← 0
-            mstatus[12:11] <= priv_reg; // MPP  ← previous mode
-            priv_reg       <= PRIV_M;   // now in Machine mode
-        end
-        //----------------------------------------------------------------------
-        // Return from Machine trap
-        //----------------------------------------------------------------------
-        else if (mret_i) begin
-            priv_reg       <= MPP;      // back to previous mode (U or M)
-            mstatus[3]     <= MPIE;     // MIE  ← MPIE
-            mstatus[7]     <= 1'b1;     // MPIE ← 1
-            mstatus[12:11] <= 2'b00;    // MPP  ← 0
-        end
-        //----------------------------------------------------------------------
-        // Explicit CSR write (from CSRRW/RS/RC…)
-        //----------------------------------------------------------------------
-        else if (csr_we_i) begin
-            case (csr_addr_i)
-                // ---------- Machine CSRs ----------
-                12'h300: mstatus  <= csr_wdata_i;
-                12'h304: mie      <= csr_wdata_i;
-                12'h305: mtvec    <= csr_wdata_i;
-                12'h340: mscratch <= csr_wdata_i;
-                12'h341: mepc     <= csr_wdata_i;
-                12'h342: mcause   <= csr_wdata_i;
-                12'h343: mtval    <= csr_wdata_i;
+// Trap Setup
+reg [31:0] csr_mstatus_q;
+reg [31:0] csr_medeleg_q;
+reg [31:0] csr_mideleg_q;
+reg [31:0] csr_mie_q;
+reg [31:0] csr_mtvec_q;
+reg [31:0] csr_mcounteren_q;
+reg [31:0] csr_mstatush_q;
+reg [31:0] csr_medelegh_q;
 
-                // ---------- User CSRs -------------
-                12'h005: utvec    <= csr_wdata_i;
-                12'h040: uscratch <= csr_wdata_i;
-                12'h041: uepc     <= csr_wdata_i;
-                12'h042: ucause   <= csr_wdata_i;
-                12'h043: utval    <= csr_wdata_i;
+// Trap Handling
+reg [31:0] csr_mscratch_q;
+reg [31:0] csr_mepc_q;
+reg [31:0] csr_mcause_q;
+reg [31:0] csr_mtval_q;
+reg [31:0] csr_mip_q;
+reg [31:0] csr_mtinst_q;
+reg [31:0] csr_mtval2_q;
 
-                // ustatus – we only model UIE/UPIE bits (0,4)
-                12'h000: begin
-                    mstatus[0] <= csr_wdata_i[0];  // UIE
-                    mstatus[4] <= csr_wdata_i[4];  // UPIE
-                end
-                12'h004: uie     <= csr_wdata_i[0]; // lowest bit only
-                default: ;
-            endcase
-        end
-    end
-end
+// Configuration
+reg [31:0] csr_menvcfg_q;
+reg [31:0] csr_menvcfgh_q;
+reg [31:0] csr_mseccfg_q;
+reg [31:0] csr_mseccfgh_q;
 
-///////////////////////////////////////////////////////////////////////////////
-// Combinational CSR read
-///////////////////////////////////////////////////////////////////////////////
+// Memory Protection
+reg [31:0] csr_pmpcfg_q     [0:15];
+reg [31:0] csr_pmpaddr_q    [0:63];
+
+// State Enable Registers
+reg [31:0] csr_mstateen_q     [0:3];
+reg [31:0] csr_mstateenh_q    [0:3];
+
+// Non-Maskable Interrupt Handling
+reg [31:0] csr_mnscratch_q;
+reg [31:0] csr_mnepc_q;
+reg [31:0] csr_mncause_q;
+reg [31:0] csr_mnstatus_q;
+
+// Counter/Timers
+reg [31:0] csr_mcycle_q;
+reg [31:0] csr_minstret_q;
+reg [31:0] csr_mhpmcounter_q  [3:31];
+reg [31:0] csr_mcycleh_q;
+reg [31:0] csr_minstreth_q;
+reg [31:0] csr_mhpmcounterh_q [3:31];
+
+// Counter Setup
+reg [31:0] csr_mcountinhibit_q;
+reg [31:0] csr_mhpmevent_q    [3:31];
+reg [31:0] csr_mhpmeventh_q   [3:31];
+
+//-----------------------------------------------------------------
+// CSR Read Port
+//-----------------------------------------------------------------
+reg [31:0] csr_rd_data_r;
 always @(*) begin
-    case (csr_addr_i)
-        // Machine CSRs
-        12'h300: csr_rdata_o = mstatus;
-        12'h304: csr_rdata_o = mie;
-        12'h305: csr_rdata_o = mtvec;
-        12'h340: csr_rdata_o = mscratch;
-        12'h341: csr_rdata_o = mepc;
-        12'h342: csr_rdata_o = mcause;
-        12'h343: csr_rdata_o = mtval;
-
-        // User CSRs
-        12'h000: csr_rdata_o = {27'b0, mstatus[4], 3'b0, mstatus[0]}; // ustatus
-        12'h004: csr_rdata_o = {31'b0, uie};                          // uie
-        12'h005: csr_rdata_o = utvec;
-        12'h040: csr_rdata_o = uscratch;
-        12'h041: csr_rdata_o = uepc;
-        12'h042: csr_rdata_o = ucause;
-        12'h043: csr_rdata_o = utval;
-        default: csr_rdata_o = 32'h0;
+    case (csr_rd_addr_i)
+    // CSR - Machine
+        `CSR_MHARTID:   csr_rd_data_r = cpu_id_i;
+        // Trap Setup
+        `CSR_MSTATUS:   csr_rd_data_r = csr_mstatus_q & `CSR_MSTATUS_MASK;
+        `CSR_MISA:      csr_rd_data_r = misa_i;
+        `CSR_MEDELEG:   csr_rd_data_r = csr_medeleg_q & `CSR_MEDELEG_MASK;
+        `CSR_MIDELEG:   csr_rd_data_r = csr_mideleg_q & `CSR_MIDELEG_MASK;
+        `CSR_MIE:       csr_rd_data_r = csr_mie_q & `CSR_MIE_MASK;
+        `CSR_MTVEC:     csr_rd_data_r = csr_mtvec_q & `CSR_MTVEC_MASK;
+        // Trap Handling
+        `CSR_MSCRATCH:  csr_rd_data_r = csr_mscratch_q & `CSR_MSCRATCH_MASK;
+        `CSR_MEPC:      csr_rd_data_r = csr_mepc_q & `CSR_MEPC_MASK;
+        `CSR_MCAUSE:    csr_rd_data_r = csr_mcause_q & `CSR_MCAUSE_MASK;
+        `CSR_MTVAL:     csr_rd_data_r = csr_mtval_q & `CSR_MTVAL_MASK;
+        `CSR_MIP:       csr_rd_data_r = csr_mip_q & `CSR_MIP_MASK;
+        // Counter/Timers
+        `CSR_MCYCLE,    
+        `CSR_MTIME:     csr_rd_data_r = csr_mcycle_q;
+        `CSR_MTIMEH:    csr_rd_data_r = csr_mcycleh_q;
+    // CSR - Supervisor
+        default:
+                        csr_rd_data_r = 32'b0;
     endcase
 end
 
-///////////////////////////////////////////////////////////////////////////////
-// PC redirect (combinational)
-//   • Trap  → mtvec
-//   • MRET  → mepc
-///////////////////////////////////////////////////////////////////////////////
-assign csr_pc_redirect_o = trap_taken_i ? mtvec :
-                           (mret_i ? mepc : 32'h0);
+assign csr_rd_data_i = csr_rd_data_r;
+assign priv_o        = csr_priv_q;
+assign mstatus_o     = csr_mstatus_q;
+assign satp_o        = csr_satp_q;
+
+//-----------------------------------------------------------------
+// CSR register next state
+//-----------------------------------------------------------------
+    // Information RO
+reg [31:0] csr_mvendorid_r;
+reg [31:0] csr_marchid_r;
+reg [31:0] csr_mimpid_r;
+reg [31:0] csr_mhartid_r;
+reg [31:0] csr_mconfigptr_r;
+    // Trap Setup
+reg [31:0] csr_mstatus_r;
+reg [31:0] csr_medeleg_r;
+reg [31:0] csr_mideleg_r;
+reg [31:0] csr_mie_r;
+reg [31:0] csr_mtvec_r;
+reg [31:0] csr_mcounteren_r;
+reg [31:0] csr_mstatush_r;
+reg [31:0] csr_medelegh_r;
+    // Trap Handling
+reg [31:0] csr_mscratch_r;
+reg [31:0] csr_mepc_r;
+reg [31:0] csr_mcause_r;
+reg [31:0] csr_mtval_r;
+reg [31:0] csr_mip_r;
+reg [31:0] csr_mtinst_r;
+reg [31:0] csr_mtval2_r;
+    // configuration
+reg [31:0] csr_menvcfg_r;
+reg [31:0] csr_menvcfgh_r;
+reg [31:0] csr_mseccfg_r;
+reg [31:0] csr_mseccfgh_r;
+    // memory protection
+reg [31:0] csr_pmpcfg_r     [0:15];
+reg [31:0] csr_pmpaddr_r    [0:63];
+    // State Enable Registers
+reg [31:0] csr_mstateen_r     [0:3];
+reg [31:0] csr_mstateenh_r    [0:3];
+    // Non-Maskable Interrupt Handling
+reg [31:0] csr_mnscratch_r;
+reg [31:0] csr_mnepc_r;
+reg [31:0] csr_mncause_r;
+reg [31:0] csr_mnstatus_r;
+    // Counter/Timers
+reg [31:0] csr_mcycle_r;
+reg [31:0] csr_minstret_r;
+reg [31:0] csr_mhpmcounter_r  [3:31];
+reg [31:0] csr_mcycleh_r;
+reg [31:0] csr_minstreth_r;
+reg [31:0] csr_mhpmcounterh_r [3:31];
+    // Counter Setup
+reg [31:0] csr_mcountinhibit_r;
+reg [31:0] csr_mhpmevent_r    [3:31];
+reg [31:0] csr_mhpmeventh_r   [3:31];
+
+wire is_exception = | exception_i;
+
+always @(*) begin
+    // privilege level
+    csr_priv_r = csr_priv_q;
+
+    // Trap Setup
+    csr_mstatus_r   = csr_mstatus_q;
+    csr_medeleg_r   = csr_medeleg_q;
+    csr_mideleg_r   = csr_mideleg_q;
+    csr_mie_r       = csr_mie_q;
+    csr_mtvec_r     = csr_mtvec_q;
+    csr_medelegh_r  = csr_medelegh_q;
+
+    // Trap Handling
+    csr_mscratch_r  = csr_mscratch_q;
+    csr_mepc_r      = csr_mepc_q;
+    csr_mcause_r    = csr_mcause_q;
+    csr_mtval_r     = csr_mtval_q;
+    csr_mip_r       = csr_mip_q;
+    csr_mip_next_r  = csr_mip_next_q;
+
+    // Counter/Timers
+    csr_mcycle_r    = csr_mcycle_q + 32'd1;
+
+    if(is_exception) begin
+        // placeholder for interruption and exception handling
+    end else begin
+        case(csr_wr_addr_i)
+        // CSR - Machine
+            // Trap Setup
+            `CSR_MSTATUS: csr_mstatus_r   = csr_wr_data_i & `CSR_MSTATUS_MASK;
+            `CSR_MEDELEG: csr_medeleg_r   = csr_wr_data_i & `CSR_MEDELEG_MASK;
+            `CSR_MIDELEG: csr_mideleg_r   = csr_wr_data_i & `CSR_MIDELEG_MASK;
+            `CSR_MIE:     csr_mie_r       = csr_wr_data_i & `CSR_MIE_MASK;
+            `CSR_MTVEC:   csr_mtvec_r     = csr_wr_data_i & `CSR_MTVEC_MASK;
+            // Trap Handling
+            `CSR_MSCRATCH:csr_mscratch_r  = csr_wr_data_i & `CSR_MSCRATCH_MASK;
+            `CSR_MEPC:    csr_mepc_r      = csr_wr_data_i & `CSR_MEPC_MASK;
+            `CSR_MCAUSE:  csr_mcause_r    = csr_wr_data_i & `CSR_MCAUSE_MASK;
+            `CSR_MTVAL:   csr_mtval_r     = csr_wr_data_i & `CSR_MTVAL_MASK;
+            `CSR_MIP:     csr_mip_r       = csr_wr_data_i & `CSR_MIP_MASK;
+            default:;
+        endcase
+    end
+
+    csr_mip_r = csr_mip_r | csr_mip_next_r;
+
+end
 
 endmodule
