edb368ec8578ce4e7420b2b9d5be7c594118fa1f
JimYang2023 <y20041112@gmail.com>
2025-08-07 17:41:40 +0800

    update lsu

--- LSU.v
+++ LSU.v
@@ -1,42 +1,14 @@
 //-----------------------------------------------------------------
 // LSU
 //-----------------------------------------------------------------
-// Input: 
-// opcode_opcode_i      : instruction value
-// opcode_rd_i          : writeback register number
-// opcode_ra_data_i     : address register value
-// opcode_rb_data_i     : data register value
-// opcode_fp_ra_data_i  : floating point address register
-// opcode_fp_rb_data_i  : floating point data register
-// cache_data_i         : cache data value
-// cache_addr_i         : cache address
-// cache_hit_i          : cache hit result
-// cache_act_i        : cache action finished
-// mem_data_rd_i        : the value of memory read data
-// mem_act_i            : memory action finfished (Load and Store)
-//
-// Output: 
-// cache_addr_o         : read and write address
-// cache_data_o         : write value
-// cache_rd_o           : cache read signal
-// cache_mask_o         : cache write mask
-// cache_wait_o         : cache finished and memoruy didn't in store instruction.
-// mem_addr_o           : send address to memory (load without cache & store)
-// mem_data_wr_o        : value of store data
-// mem_valid_o          : memory output signal is valid
-// mem_rd_o             : memory read signal
-// mem_wr_o             : memory write signal
-// writeback_value_o    : writeback result
-// writeback_rd_o       : writeback register number
-// writeback_valid_o    : writeback signal is valid
-//-----------------------------------------------------------------
 
-// Include
-`include"LSU_def.v"
-`include"riscv_def.v"
-
+`include"riscv_defs.v"
 
 module lsu
+#(
+     parameter LENGTH   = 16
+    ,parameter DEPTH    = 4
+)
 (   
      input           clk_i
     ,input           rst_i
@@ -44,30 +16,21 @@ module lsu
     ,input   [ 4:0]  opcode_rd_i
     ,input   [31:0]  opcode_ra_data_i
     ,input   [31:0]  opcode_rb_data_i
-    ,input   [31:0]  opcode_fp_ra_data_i
-    ,input   [31:0]  opcode_fp_rb_data_i
     ,input           opcode_valid_i
     
-    ,input   [31:0]  cache_data_i
-    ,input           cache_hit_i
-    ,input           cache_act_i
 
-    ,input   [31:0]  mem_data_rd_i
-    ,input           mem_act_i
-    ,input           mem_load_fault  
-    ,input           mem_store_fault
+    ,input  [31:0]  mmu_value_i
+    ,input          mmu_valid_i
+    ,input          mmu_load_fault
+    ,input          mmu_store_fault
 
-    ,output  [31:0]  cache_addr_o
-    ,output  [31:0]  cache_data_o
-    ,output          cache_rd_o
-    ,output  [ 3:0]  cache_mask_o
-    ,output          cache_wait_o
-
-    ,output  [31:0]  mem_addr_o
-    ,output  [31:0]  mem_data_wr_o
-    ,output          mem_rd_o
-    ,output  [ 3:0]  mem_wr_o
-    ,output          mem_cachable_o 
+    ,output  [31:0]  mmu_addr_o
+    ,output  [31:0]  mmu_data_o
+    ,output          mmu_rd_o
+    ,output  [ 3:0]  mmu_wr_o
+    ,output          mmu_dflush_o
+    ,output          mmu_dinvalidate_o
+    ,output          mmu_dwriteback_o
 
     ,output  [31:0]  writeback_value_o
     ,output  [ 4:0]  writeback_rd_o
@@ -75,24 +38,13 @@ module lsu
     ,output          stall_o
 
     ,output  [5:0]   exception_o
-
-    ,output          dflush_o
-    ,output          dinvalidate_o
-    ,output          dwriteback_o
 );
 
 // --------------------------------------------
 //  Parameter Declaration
 // --------------------------------------------
 
-// Queue
 localparam DATASIZE = 78;
-localparam LENGTH   = 16;
-localparam DEPTH    = 4;
-
-// FSM
-localparam FSM_INST  = 1'b0;
-localparam FSM_POP   = 1'b1;
 
 // --------------------------------------------
 //  Register Declaration
@@ -111,10 +63,6 @@ reg [ 3:0] mem_wr_r;
 // Queue
 reg [ DATASIZE-1:0] data_q_i;
 
-// Writeback
-reg [31:0]  mem_cache_rd_value_r;
-reg [31:0]  writeback_value_r;
-
 // FSM & ACT
 reg     fsm_state;
 reg     cache_act_r;
@@ -132,11 +80,9 @@ wire st_inst;
 
 wire lb_inst, lh_inst, lw_inst;
 wire sb_inst, sh_inst, sw_inst;
-wire sign_inst, fp_inst;
+wire sign_inst;
 
-wire flw_inst, fsw_inst;
-
-wire csr_inst;
+wire csrrw_inst;
 
 // Queue
 wire                is_load_i;
@@ -153,11 +99,6 @@ wire                resp_is_load;
 wire        [ 3:0]  resp_wr;
 wire        [ 4:0]  resp_rd;
 
-// FSM
-wire    fsm_finished;
-wire    is_inst;
-wire    is_pop;
-
 // --------------------------------------------
 //  Error Detection
 // --------------------------------------------
@@ -170,8 +111,8 @@ assign addr_unaligned = unaligned_r;
 
 assign exception_o = (addr_unaligned && mem_rd_r)?`EXCEPTION_MISALIGNED_LOAD: 
                      (addr_unaligned && (|mem_wr_r))?`EXCEPTION_MISALIGNED_STORE:
-                     (resp_is_load && mem_load_fault)?`EXCEPTION_FAULT_LOAD:
-                     ((|resp_wr) && mem_store_fault)?`EXCEPTION_FAULT_STORE:
+                     (resp_is_load && mmu_load_fault)?`EXCEPTION_FAULT_LOAD:
+                     ((|resp_wr) && mmu_store_fault)?`EXCEPTION_FAULT_STORE:
                      5'h0;
 
 always @(*)begin
@@ -186,32 +127,26 @@ always @(*)begin
 end
 
 
-
 // --------------------------------------------
 //  Opcode 
 // --------------------------------------------
 
-assign ld_inst = opcode_valid_i && (lb_inst || lh_inst || lw_inst || flw_inst);
-assign st_inst = opcode_valid_i && (sb_inst || sh_inst || sw_inst || fsw_inst);
+assign ld_inst = opcode_valid_i && (lb_inst || lh_inst || lw_inst);
+assign st_inst = opcode_valid_i && (sb_inst || sh_inst || sw_inst);
 
 assign lb_inst = ((opcode_opcode_i & `INST_LB_MASK) == `INST_LB) || ((opcode_opcode_i & `INST_LBU_MASK) == `INST_LBU);
 assign lh_inst = ((opcode_opcode_i & `INST_LH_MASK) == `INST_LH) || ((opcode_opcode_i & `INST_LBU_MASK) == `INST_LHU);
-assign lw_inst = ((opcode_opcode_i & `INST_LW_MASK) == `INST_LW) || ((opcode_opcode_i & `INST_LBU_MASK) == `INST_LWU) || flw_inst;
+assign lw_inst = ((opcode_opcode_i & `INST_LW_MASK) == `INST_LW) || ((opcode_opcode_i & `INST_LBU_MASK) == `INST_LWU);
 
 assign sb_inst = ((opcode_opcode_i & `INST_LB_MASK) == `INST_SB);
 assign sh_inst = ((opcode_opcode_i & `INST_LH_MASK) == `INST_SH);
-assign sw_inst = ((opcode_opcode_i & `INST_LW_MASK) == `INST_SW) || fsw_inst;
+assign sw_inst = ((opcode_opcode_i & `INST_LW_MASK) == `INST_SW);
 
 assign sign_inst = ((opcode_opcode_i & `INST_LB_MASK) == `INST_LB) ||
                    ((opcode_opcode_i & `INST_LH_MASK) == `INST_LH) ||
                    ((opcode_opcode_i & `INST_LW_MASK) == `INST_LW);
 
-assign flw_inst = ((opcode_opcode_i & `INST_FLW_MASK) == `INST_FLW);
-assign fsw_inst = ((opcode_opcode_i & `INST_FLW_MASK) == `INST_FSW);
-
-assign fp_inst = (flw_inst || fsw_inst);
-
-assign csr_inst = ((opcode_opcode_i & `INST_CSR_MASK) == `INST_CSRRW);
+assign csrrw_inst = ((opcode_opcode_i & `INST_CSRRW_MASK) == `INST_CSRRW);
 
 // CSRRW Instruction
 wire dflush, dwriteback, dinvalidate;
@@ -220,32 +155,32 @@ assign dflush       = opcode_valid_i && (opcode_opcode_i[31:20] == `CSR_DFLUSH);
 assign dwriteback   = opcode_valid_i && (opcode_opcode_i[31:20] == `CSR_DWRITEBACK);
 assign dinvalidate  = opcode_valid_i && (opcode_opcode_i[31:20] == `CSR_DINVALIDATE);
 
-assign dflush_o = dflush && csr_inst;
-assign dwriteback_o = dwriteback && csr_inst;
-assign dinvalidate_o = dinvalidate && csr_inst;
+assign mmu_dflush_o = dflush && csrrw_inst;
+assign mmu_dwriteback_o = dwriteback && csrrw_inst;
+assign mmu_dinvalidate_o = dinvalidate && csrrw_inst;
 
-always @(*)begin
+always @(*)
+begin
     ra_data = 32'b0;
     rb_data = 32'b0;
 
-    if(fp_inst)begin
-        ra_data = opcode_fp_ra_data_i;
-        rb_data = opcode_fp_rb_data_i;
-    end else begin
-        ra_data = opcode_ra_data_i;
-        rb_data = opcode_rb_data_i;
-    end
+    ra_data = opcode_ra_data_i;
+    rb_data = opcode_rb_data_i;    
 end
 
 // --------------------------------------------
-//  Memory
+//  MMU
 // --------------------------------------------
 
-assign mem_data_wr_o    = resp_data;
-assign mem_addr_o       = resp_addr;
-assign mem_rd_o         = (is_inst && resp_valid_o)?resp_is_load:4'h0;
-assign mem_wr_o         = (is_inst && resp_valid_o)?resp_wr:4'h0;
-assign mem_cachable_o   = cache_hit_r && cache_act_r && resp_is_load; 
+assign mmu_addr_o   = resp_addr;
+assign mmu_data_o   = resp_data;
+assign mmu_valid_o  = resp_valid_o;
+assign mmu_rd_o     = resp_is_load;
+assign mmu_wr_o     = resp_wr;
+
+// --------------------------------------------
+//  Input Address & Data Control
+// --------------------------------------------
 
 always @(*)begin
     mem_addr_r = 32'b0;
@@ -309,17 +244,6 @@ always @(*)begin
         mem_wr_r = 4'b0000;
 end
 
-
-// --------------------------------------------
-//  Cache
-// --------------------------------------------
-
-assign cache_addr_o = resp_addr;
-assign cache_data_o = resp_data;
-assign cache_rd_o   = is_inst && resp_is_load;
-assign cache_mask_o = (is_inst && resp_valid_o)?resp_wr:4'h0;
-assign cache_wait_o = is_inst && (cache_act_r || cache_act_i) && ~mem_act_r;   
-
 // --------------------------------------------
 //  Queue 
 // --------------------------------------------
@@ -340,7 +264,7 @@ lsu_queue #(
     .push_i((mem_rd_r || (|mem_wr_r) ) && resp_accept_o && opcode_valid_i),
     .accept_o(resp_accept_o),
 
-    .pop_i(is_pop && resp_data_o),
+    .pop_i(mmu_valid_i && resp_valid_o),
     .data_o(resp_data_o),
     .valid_o(resp_valid_o)      
 );
@@ -366,102 +290,44 @@ assign stall_o = (~resp_accept_o && (ld_inst || st_inst)) || (dwriteback || dinv
 //  Writeback
 // --------------------------------------------
 
+reg [31:0] writeback_value_r;
+
 assign writeback_value_o = writeback_value_r;
-assign writeback_valid_o = is_pop && resp_is_load;
+assign writeback_valid_o = mmu_valid_i;
 assign writeback_rd_o    = resp_rd;
 
-always @(posedge clk_i or negedge rst_i)begin
-    if(~rst_i)begin
-        cache_act_r <= 0;
-        cache_hit_r <= 0;
-        mem_act_r   <= 0;
-    end else begin
-        if(is_pop)begin
-            cache_act_r <= 0;
-            cache_hit_r <= 0;
-            mem_act_r   <= 0;
-        end else begin
-            cache_act_r <= (cache_act_r || cache_act_i);
-            cache_hit_r <= (cache_hit_r || cache_hit_i);
-            mem_act_r   <= (mem_act_r   || mem_act_i  );
-        end    
-    end
-end
-
 always @(*)begin
-    mem_cache_rd_value_r = 32'b0;
     writeback_value_r = 32'b0;
 
-    // mem_cache_rd_value select
-    if (resp_is_load)begin
-        if(cache_hit_i && cache_act_i)
-            mem_cache_rd_value_r = cache_data_i;
-        else if(mem_act_i && ~cache_hit_r)
-            mem_cache_rd_value_r = mem_data_rd_i;
-        else 
-            mem_cache_rd_value_r = 32'b0;
-    end else 
-        mem_cache_rd_value_r = 32'b0;
-
-    // writeback result calculation
-    if (resp_is_load && is_pop)begin
-        if (resp_lb)
-        begin
-            case (resp_addr[1:0])
-            2'h3: writeback_value_r = {24'b0, mem_cache_rd_value_r[31:24]};
-            2'h2: writeback_value_r = {24'b0, mem_cache_rd_value_r[23:16]};
-            2'h1: writeback_value_r = {24'b0, mem_cache_rd_value_r[15:8]};
-            2'h0: writeback_value_r = {24'b0, mem_cache_rd_value_r[7:0]};
-            endcase
-
-            if (resp_signed && writeback_value_r[7])
-                writeback_value_r = {24'hFFFFFF, writeback_value_r[7:0]};
-        end
-        else if (resp_lh)
-        begin
-            if (resp_addr[1])
-                writeback_value_r = {16'h0, mem_cache_rd_value_r[31:16]};
-            else
-                writeback_value_r = {16'h0, mem_cache_rd_value_r[15:0]};
-
-            if (resp_signed && writeback_value_r[15])
-                writeback_value_r = {16'hFFFF, writeback_value_r[15:0]};
-        end
-        else if(resp_lw)
-            writeback_value_r = mem_cache_rd_value_r;
-    end else
-        writeback_value_r = 32'h0;
-end
-
-// --------------------------------------------
-//  FSM
-// --------------------------------------------
-
-wire load_finished;
-wire store_finished;
-
-assign load_finished    = (cache_act_r || cache_act_i) && (mem_act_r || mem_act_i || cache_hit_i);
-assign store_finished   = (cache_act_r || cache_act_i) && (mem_act_r || mem_act_i);
-
-assign fsm_finished = (resp_is_load && load_finished) || (~resp_is_load && store_finished);
-assign is_inst  = (fsm_state == FSM_INST);
-assign is_pop   = (fsm_state == FSM_POP);
-
-always @(posedge clk_i or negedge rst_i)begin
-    if(~rst_i)begin
-        fsm_state <= FSM_INST;
-    end else begin
-        case(fsm_state)
-        FSM_INST:
-        begin
-            if(fsm_finished) 
-                fsm_state <= FSM_POP;
-            else 
-                fsm_state <= FSM_INST;
-        end            
-        FSM_POP: fsm_state <= FSM_INST;
+    if (resp_lb && mmu_valid_i)
+    begin
+        case (resp_addr[1:0])
+        2'h3: writeback_value_r = {24'b0, mmu_value_i[31:24]};
+        2'h2: writeback_value_r = {24'b0, mmu_value_i[23:16]};
+        2'h1: writeback_value_r = {24'b0, mmu_value_i[15:8]};
+        2'h0: writeback_value_r = {24'b0, mmu_value_i[7:0]};
         endcase
+
+        if (resp_signed && writeback_value_r[7])
+            writeback_value_r = {24'hFFFFFF, writeback_value_r[7:0]};
     end
+    else if (resp_lh && mmu_valid_i)
+    begin
+        case(resp_addr[1])
+        1'b0: writeback_value_r = {16'h0, mmu_value_i[31:16]};
+        1'b1: writeback_value_r = {16'h0, mmu_value_i[15:0]};
+        default: writeback_value_r = 32'b0;
+        endcase 
+
+        if (resp_signed && mmu_value_i[15])
+            writeback_value_r = {16'hFFFF, writeback_value_r[15:0]};
+    end else if(resp_lw && mmu_valid_i)
+    begin
+        writeback_value_r = mmu_value_i;
+    end
+    else
+        writeback_value_r = 32'h0;
+
 end
 
 endmodule
@@ -538,3 +404,4 @@ always @(posedge clk_i or negedge rst_i) begin
 end
 
 endmodule
+
