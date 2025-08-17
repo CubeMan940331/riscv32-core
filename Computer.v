ff9f1ebdd82f280fbf754001c022024d958c8b99
nototria <humphrey0709@gmail.com>
2025-08-17 23:17:25 +0800

    initial commit of integrated RAM

--- Computer.v
+++ Computer.v
@@ -13,20 +13,18 @@ wire [31:0] d_mem_addr;
 wire [31:0] d_mem_wr_data;
 wire [31:0] d_mem_rd_data;
 
-DataMemory m_DataMemory(
-    .rst_n      (rst_n),
-    .clk        (clk),
-    .wr_en      (d_mem_wr_en),
-    .rd_en      (d_mem_rd_en),
-    .ctrl       (d_mem_ctrl),
-    .address    (d_mem_addr),
-    .data_i     (d_mem_wr_data),
-    .data_o     (d_mem_rd_data)
-);
-
-InstructionMemory m_InstMem(
-    .address(i_mem_addr),
-    .inst(inst)
+TCM_wrapper m_TCM_wrapper(
+    .clk(clk),
+    
+    .i_addr(i_mem_addr),
+    .i_data(inst),
+    
+    .d_rd_mode(d_mem_ctrl),
+    .d_rd_en(d_mem_rd_en),
+    .d_rd_data(d_mem_rd_data),
+    .d_addr(d_mem_addr),
+    .d_wr_valid(d_mem_wr_en),
+    .d_wr_data(d_mem_wr_data)
 );
 
 PipelineCPU m_core0(
