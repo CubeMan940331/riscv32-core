af3fb293b9b61ee48465ed27b748872ec865571b
nototria <humphrey0709@gmail.com>
2025-08-20 21:33:08 +0800

    fix all modules warning on vivado

--- Decode.v
+++ Decode.v
@@ -22,49 +22,47 @@ module Decode (
     output reg [31:0] imm_o,
 
     // WB stage
-    output reg reg_wr_en_o,
-    output reg freg_wr_en_o,
-    output reg [2:0] reg_w_sel_o, // 0: pc_p4, 1: ALU, 2: mem, 3:csr, 4: FPU, 5: bypass, 6: MUL_DIV_top
+    output reg_wr_en_o,
+    output freg_wr_en_o,
+    output [2:0] reg_w_sel_o, // 0: pc_p4, 1: ALU, 2: mem, 3:csr, 4: FPU, 5: bypass, 6: MUL_DIV_top
     
     // LSU
-    output reg mem_wr_en_o,
-    output reg mem_rd_en_o,
-    output reg [3:0] mem_ctrl_o,
+    output mem_wr_en_o,
+    output mem_rd_en_o,
+    output [3:0] mem_ctrl_o,
     
     // Branch
-    output reg is_j_o,
-    output reg is_br_o,
-    output reg [2:0] cmp_op_o,
+    output is_j_o,
+    output is_br_o,
+    output [2:0] cmp_op_o,
     
     // ALU
-    output reg [3:0] ALU_ctrl_o,
-    output reg ALU_sel1_o, // 0: PC, 1: rs1
-    output reg ALU_sel2_o, // 0: rs2, 1: imm
+    output [3:0] ALU_ctrl_o,
+    output ALU_sel1_o, // 0: PC, 1: rs1
+    output ALU_sel2_o, // 0: rs2, 1: imm
 
     // MUL/DIV
-    output reg is_MUL_DIV_o,
-    output reg [2:0] MUL_DIV_ctrl_o,
+    output is_MUL_DIV_o,
+    output [2:0] MUL_DIV_ctrl_o,
 
     // CSR
-    output reg is_csr_o,
-    output reg [2:0] csr_op_o,
-    output reg is_csr_imm_o, // is csr[r w]i
+    output is_csr_o,
+    output [2:0] csr_op_o,
+    output is_csr_imm_o, // is csr[r w]i
 
     // FPU
-    output reg is_fpu_o,
-    output reg FPU_sel1_o, // 0: fs1, 1: rs1
+    output is_fpu_o,
+    output FPU_sel1_o, // 0: fs1, 1: rs1
 
     // Bypass
-    output reg [1:0] bypass_sel_o,
+    output [1:0] bypass_sel_o,
 
     // Fence
-    output reg fetch_invalid_o,
+    output fetch_invalid_o,
 
     output is_impl_o
 );
 parameter CSR_FRM = 12'h002;
-
-
 
 // Immediate Generation
 always @(*)begin
@@ -146,17 +144,8 @@ ID_Reg m_ID_Reg(
     .pc_p4_o(ID_pc_p4_out),
     .inst_o(ID_inst_out)
 );
-assign pc_valid_o = ID_pc_valid_out;
-assign pc_o = ID_pc_out;
-assign pc_p4_o = ID_pc_p4_out;
-assign inst_o = ID_inst_out;
 
 wire [6:0] opcode = ID_inst_out[6:0];
-
-assign rs1_o = ID_inst_out[19:15];
-assign rs2_o = ID_inst_out[24:20];
-assign rs3_o = ID_inst_out[31:27];
-assign rd_o  = ID_inst_out[11:07];
 
 Control m_Control(
     .inst(ID_inst_out),
@@ -214,6 +203,14 @@ assign is_fpu_o = is_fpu;
 assign FPU_sel1_o = FPU_sel1;
 assign bypass_sel_o = bypass_sel;
 assign fetch_invalid_o = fetch_invalid;
+assign pc_valid_o = ID_pc_valid_out;
+assign pc_o = ID_pc_out;
+assign pc_p4_o = ID_pc_p4_out;
+assign inst_o = ID_inst_out;
+assign rs1_o = ID_inst_out[19:15];
+assign rs2_o = ID_inst_out[24:20];
+assign rs3_o = ID_inst_out[31:27];
+assign rd_o  = ID_inst_out[11:07];
 
 always@(*)begin
     csr_addr_o = ID_inst_out[31:20];
